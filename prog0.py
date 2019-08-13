5b18c5795e492f24aa2d3076b84eea52b5f40725
saidileep1 <45943131+saidileep1@users.noreply.github.com>
2019-08-13 14:14:13 +0530

    Update prog0.py

--- prog0.py
+++ prog0.py
@@ -1,7 +1,7 @@
-a=input("enter number")
+a=int(input("enter number"))
 if a<0:
-    print"number is negative"
+    print("number is negative")
 elif a>0:
-    print"number is positive"
+    print("number is positive")
 else:
-    print"number is zero"
+    print("number is zero")
